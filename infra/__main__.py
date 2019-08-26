747e4d6fd291376c2ae6750450e06f37467eb1a2
Carlos Nunez <carlosonunez@noreply.github.com>
2019-08-26 00:12:00 -0500

    Add IAM stuff

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,6 +1,16 @@
-import pulumi
-from pulumi_aws import s3
+import pulumi, os, json
+from pulumi_aws import s3, iam
+
+for required_env_var in ['SERVERLESS_BUCKET_NAME']:
+    if os.environ.get(required_env_var) == None:
+        raise "Please define " + required_env_var
+
+minimal_serverless_iam_policy_file = open('files/serverless_iam_policy.json')
+minimal_serverless_iam_policy = minimal_serverless_iam_policy_file.read()
 
 config = pulumi.Config('gmail-expensify-forwarder')
-
-serverless_bucket = s3.bucket(os.environ['SERVERLESS_BUCKET_NAME'])
+serverless_bucket = s3.Bucket(os.environ.get('SERVERLESS_BUCKET_NAME'))
+serverless_iam_user = iam.User('gmail-expensify-forwarder-serverless-user')
+serverless_iam_user_policy = iam.UserPolicy('gmail-expensify-forwarder-serverless-user-policy',
+                                            policy=minimal_serverless_iam_policy,
+                                            user=serverless_iam_user.name)
