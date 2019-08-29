ef492c30fe2b33ea408de76b4a41642fe3998a7b
Carlos Nunez <carlosonunez@noreply.github.com>
2019-08-29 18:31:20 -0500

    We need to apply the IAM policy towards a group b/c of its size

--- infra/__main__.py
+++ infra/__main__.py
@@ -5,12 +5,14 @@ for required_env_var in ['SERVERLESS_BUCKET_NAME']:
     if os.environ.get(required_env_var) == None:
         raise "Please define " + required_env_var
 
-minimal_serverless_iam_policy_file = open('files/serverless_iam_policy.json')
-minimal_serverless_iam_policy = minimal_serverless_iam_policy_file.read()
-
-config = pulumi.Config('gmail-expensify-forwarder')
+minimal_serverless_iam_policy_json = json.loads(
+    open('files/serverless_iam_policy.json', 'r').read())
 serverless_bucket = s3.Bucket(os.environ.get('SERVERLESS_BUCKET_NAME'))
+serverless_iam_group = iam.Group('gmail-expensify-serverless-group')
+serverless_iam_user_policy = iam.GroupPolicy('gmail-expensify-forwarder-serverless-user-policy',
+                                             policy=json.dumps(minimal_serverless_iam_policy_json),
+                                             group=serverless_iam_group.name)
 serverless_iam_user = iam.User('gmail-expensify-forwarder-serverless-user')
-serverless_iam_user_policy = iam.UserPolicy('gmail-expensify-forwarder-serverless-user-policy',
-                                            policy=minimal_serverless_iam_policy,
-                                            user=serverless_iam_user.name)
+_ = iam.GroupMembership('gmail-expensify-forwarder-serverless-group-memberships',
+                        group=serverless_iam_group,
+                        users=[serverless_iam_user])
