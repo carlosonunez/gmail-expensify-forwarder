6af26edfe5118121bc3cd7f0284785306c8434d5
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-03 01:26:26 -0500

    Add script for seeding the SSM store for first-time runs.

---
+++ scripts/seed_aws_ssm.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+AWS_SSM_NAMESPACE='/gmail-expensify-forwarder'
+
+if ! test -f /.dockerenv
+then
+  >&2 echo "ERROR: This script is meant to be run from within Docker. \
+Use the 'seed-aws-ssm' Docker Compose service to run this script."
+  exit 1
+fi
+
+seed_gmail_credentials() {
+  if ! test -f "$CREDENTIALS_PATH"
+  then
+    >&2 echo "ERROR: Couldn't find Gmail credentials; store them here: $CREDENTIALS_PATH"
+    exit 1
+  fi
+
+  aws ssm put-parameter --name "${AWS_SSM_NAMESPACE}/credentials" \
+    --description "Gmail credentials to use" \
+    --value "$(cat "$CREDENTIALS_PATH")" \
+    --type "String" \
+    --overwrite \
+    --tier 'Standard'
+}
+
+seed_gmail_tokens() {
+  if ! test -f "$TOKEN_PATH"
+  then
+    >&2 echo "ERROR: Couldn't find Gmail tokens; run forwarder locally, then store \
+the tokens here: $TOKEN_PATH"
+    exit 1
+  fi
+
+  aws ssm put-parameter --name "${AWS_SSM_NAMESPACE}/tokens" \
+    --description "Authenticated Gmail tokens to use" \
+    --value "$(cat "$TOKEN_PATH")" \
+    --overwrite \
+    --type "String" \
+    --tier 'Standard'
+}
+
+seed_last_run_time() {
+  aws ssm put-parameter --name "${AWS_SSM_NAMESPACE}/forwarder_last_finished_time_secs" \
+    --value "$(date +%s)" \
+    --overwrite \
+    --type "String" \
+    --tier 'Standard'
+}
+
+seed_last_run_time && \
+  seed_gmail_credentials && \
+  seed_gmail_tokens
