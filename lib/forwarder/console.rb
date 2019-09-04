d2d07e7cf495725911d6da1f90a3fa0efddda025
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-04 00:52:08 -0500

    Add support for logs with timestamps.

--- lib/forwarder/console.rb
+++ lib/forwarder/console.rb
@@ -1,5 +1,10 @@
 module Forwarder
   class Console
+    def self.use_timestamps?
+      !ENV['ENABLE_LOG_TIMESTAMPS'].nil? and \
+        ENV['ENABLE_LOG_TIMESTAMPS'].downcase == 'true'
+    end
+
     def self.show_message(type, msg)
       type_color_kvp = {
         :debug => :cyan,
@@ -11,7 +16,12 @@ module Forwarder
         :color => type_color_kvp[type.to_sym],
         :mode => :bold
       )
-      puts "#{colorful_type}: #{msg}"
+      if use_timestamps?
+        current_time = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+        puts "[#{current_time}] [#{colorful_type}] #{msg}"
+      else
+        puts "#{colorful_type}: #{msg}"
+      end
     end
 
     def self.show_error_message(msg)
