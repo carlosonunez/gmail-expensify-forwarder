3a27be09ce175dae8fabd398ecaf303f9781deb2
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-19 21:34:05 -0400

    Fix typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ for command in vendor deploy-infra deploy-functions
 do
   >&2 echo "INFO: Running stage '$command'"
   compose_file_to_use=$(grep -Hl "${command}:" docker-compose*yml)
-  "${DOCKER_COMPOSE_BIN}" -compose -f $compose_file_to_use run --rm "${command}" || exit 1
+  "${DOCKER_COMPOSE_BIN}" -f $compose_file_to_use run --rm "${command}" || exit 1
 done
