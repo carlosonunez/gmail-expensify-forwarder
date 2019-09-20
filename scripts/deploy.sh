a6c2bb9c2ec28d16d97259fb21cf98bc6cc6e5f4
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-19 21:21:07 -0400

    Setting PATH didn't seem to work either.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+DOCKER_COMPOSE_BIN=${DOCKER_COMPOSE_LOCATION:-/usr/local/bin}/docker-compose
 for command in vendor deploy-infra deploy-functions
 do
   >&2 echo "INFO: Running stage '$command'"
   compose_file_to_use=$(grep -Hl "${command}:" docker-compose*yml)
-  docker-compose -f $compose_file_to_use run --rm "${command}" || exit 1
+  "${DOCKER_COMPOSE_BIN}" -compose -f $compose_file_to_use run --rm "${command}" || exit 1
 done
