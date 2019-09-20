0286016bc102fd30aff61d561d5b1f1f79d75d33
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-20 14:09:09 -0500

    Prevent needless deploys by checking files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
+
+check_if_new_deploy_is_needed() {
+  items_that_would_trigger_new_commit=$(cat <<-ITEMS
+bin/
+lib/
+infra/
+Dockerfile
+Gemfile
+serverless.yml
+env.gpg
+ITEMS
+)
+  for item in "$items_that_would_trigger_new_commit"
+  do
+    if grep -q -- "$item" < <(git diff --stat "${GITHUB_SHA:-HEAD}~1")
+    then
+      >&2 echo "INFO: Since this file or directory was changed, we will re-deploy: $item"
+      return 0
+    fi
+  done
+  return 1
+}
+
 DOCKER_COMPOSE_BIN=${DOCKER_COMPOSE_LOCATION:-/usr/local/bin}/docker-compose
+
+if ! check_if_new_deploy_is_needed
+then
+  >&2 echo "INFO: No files or directories that would trigger a new deploy \
+have been detected in this commit. Doing nothing."
+  exit 0
+fi
+
 for command in vendor deploy-infra deploy-functions
 do
   >&2 echo "INFO: Running stage '$command'"
