2356ea351674f965e9683a039c8915292c8d5da7
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-20 13:46:31 -0500

    Enable support for dry runs.

--- lib/forwarder/gmail/send.rb
+++ lib/forwarder/gmail/send.rb
@@ -5,6 +5,10 @@ module Forwarder
     class Send
       def self.send_email_raw(id, raw_message_as_base64)
         begin
+          if !ENV['DRY_RUN'].nil? and ENV['DRY_RUN'].downcase == 'true'
+            Console.show_debug_message 'Dry run is on; no messages will be sent.'
+            return
+          end
           Console.show_debug_message "Sending email with id '#{id}'"
           gmail_service = GmailService.new
           _ = gmail_service.service.send_user_message('me',
