09fcedc01f5080219dcfe08b122823685a0f41df
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-29 22:40:11 -0500

    Set the LOAD_PATH so that it can find Forwarder.

--- bin/authenticate_into_gmail.rb
+++ bin/authenticate_into_gmail.rb
@@ -1,4 +1,8 @@
 # This script is used during setup to generate tokens.
+$LOAD_PATH.unshift('./lib')
+if Dir.exist? './vendor'
+  $LOAD_PATH.unshift('./vendor/bundle/gems/**/lib')
+end
 require 'forwarder'
 
 raise "Define the credentials path" if ENV['CREDENTIALS_PATH'].nil?
