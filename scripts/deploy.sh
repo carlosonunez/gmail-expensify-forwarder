80fe11323e6c738a0a45dbe742113bcb41899928
Carlos Nunez <carlosonunez@noreply.github.com>
2019-11-10 23:01:35 -0600

    Search the HEAD commit for files requiring redeployment, and add '__deploy__' keyword for forcing commits

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,18 +10,23 @@ Gemfile
 serverless.yml
 env.gpg
 ITEMS
-  deploy_keyword_to_look_for="__nodeploy__"
+  deploy_keyword_to_look_for="__deploy__"
+  nondeploy_keyword_to_look_for="__nodeploy__"
 )
   for item in $items_that_would_trigger_new_commit
   do
-    if grep -q -- "$item" < <(git diff --stat "${GITHUB_SHA:-HEAD}~1")
+    if grep -q -- "$item" < <(git diff --stat "${GITHUB_SHA:-HEAD}")
     then
       >&2 echo "INFO: Since this file or directory was changed, we will re-deploy: $item"
       return 0
-    elif grep -q -- "$deploy_keyword_to_look_for" < <(git log -1 --pretty="%s")
+    elif grep -q -- "$nondeploy_keyword_to_look_for" < <(git log -1 --pretty="%s")
     then
       >&2 echo "INFO: This commit was flagged for non-deployment."
       return 1
+    elif grep -q -- "$deploy_keyword_to_look_for" < <(git log -1 --pretty="%s")
+    then
+      >&2 echo "INFO: A force-deployment has been triggered."
+      return 0
     fi
   done
   return 1
